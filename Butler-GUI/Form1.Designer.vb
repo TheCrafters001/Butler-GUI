f8836d47d9a6a52164c97920d374d5f83ea7a3bf
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2026-03-23 16:45:25 -0400

    Update Window & Add AutoUpdater

--- Butler-GUI/Form1.Designer.vb
+++ Butler-GUI/Form1.Designer.vb
@@ -22,10 +22,19 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        SuspendLayout()
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
-        Text = "Form1"
+        ClientSize = New Size(682, 605)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Butler GUI"
+        ResumeLayout(False)
     End Sub
 
 End Class
